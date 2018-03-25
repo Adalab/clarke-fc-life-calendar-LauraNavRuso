cf902edc78700aab5c5ae2c14a87f06faf57972f
LauraNavRuso <laura.nav.ruso@gmail.com>
2018-03-25 01:21:13 +0100

    add map with 365 days

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import DayCard from './DayCard';
+
+class App extends Component {
+  render() {
+
+    let dayOrderInYear= [];
+
+    for (let i = 1; i < 366; i++) {
+      dayOrderInYear.push(i);
+    }
+
+    console.log(dayOrderInYear);
+
+
+
+    return (
+      <div className="App">
+
+        <header className="App-header">
+          <h1 className="App-title">Good/bad-day calendar</h1>
+        </header>
+
+        <div className="access-to-editionView">+
+
+
+        </div>
+
+
+        <div className="calendar">
+
+
+
+          {dayOrderInYear.map(x => (
+            `Me gusta el ${x}`
+          ))}
+
+
+
+
+          <div className="calendar-day">
+            hola
+          </div>
+
+          <div className="calendar-day">
+            hola
+          </div>
+
+          <div className="calendar-day">
+            hola
+          </div>
+
+          <div className="calendar-day">
+            hola
+          </div>
+        </div>
+
+
+
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// dayOrderInYear.map(x => (
+//   <DayCard key={x}   />
+//
+// ))
---
+++ src/DayCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class DayCard extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <header className="App-header">
+          <h1 className="App-title">Good/bad-day calendar</h1>
+        </header>
+
+        <div className="access-to-editionView">+
+
+
+        </div>
+
+
+    
+      </div>
+    );
+  }
+}
+
+export default DayCard;
